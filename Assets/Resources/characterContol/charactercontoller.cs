166ce642a85dccce640757a8de69327d1b6d4589
aaamwh <mwhao1204>
2020-12-01 06:21:51 +0800

    initial
    initial

---
+++ Assets/Resources/characterContol/charactercontoller.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class charactercontoller : MonoBehaviour
+{
+    private Rigidbody2D rb2d;
+    public float jumpForce;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb2d = GetComponent<Rigidbody2D>();
+
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        
+    }
+}
